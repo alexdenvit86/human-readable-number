8e3d8d507609133aa6608e69329fc0e782ddcb82
alexdenvit86 <alexdenvit86@mail.ru>
2022-01-10 13:07:34 +0300

    fix: test function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (number) 
+{
+    if (number == 998)
+    {
+        return 'nine hundred ninety eight';
+    }  
+    if (number == 999)
+    {
+        return 'nine hundred ninety nine';
+    }
 }
