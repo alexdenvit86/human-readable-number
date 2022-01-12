3a429b88c4023a4e73fb231f780fb3dfdb886315
alexdenvit86 <alexdenvit86@mail.ru>
2022-01-12 13:17:45 +0300

    fix: update function toReadable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 module.exports = function toReadable (number) 
 {
-    if (number == 998)
+    const oneArray = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+                'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tenArray = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number == 0)
     {
-        return 'nine hundred ninety eight';
-    }  
-    if (number == 999)
+        return 'zero';
+    }
+    if (number < 20) 
     {
-        return 'nine hundred ninety nine';
+        return `${oneArray[number]}`;
+    }
+    if (number < 100) 
+    {
+        if ((number % 10) == 0) 
+        {
+            return `${tenArray[Math.floor(number / 10)]}`;
+        }
+        else
+        {
+            return `${tenArray[Math.floor(number / 10)]} ${oneArray[number % 10]}`;
+        }
+    } 
+    if (number < 1000)
+    {
+        if ((number % 100) == 0)
+        {
+            return `${oneArray[Math.floor(number / 100)]} hundred`;
+        }
+        else
+        {
+            return `${oneArray[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`;
+        }
     }
 }
